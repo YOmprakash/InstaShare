b2471cde6842baff19ea22d1806b123bfc5b8140
Omprakash <omprakashyamavaram@mail.com>
2024-02-28 06:27:48 +0000

    eight commit

--- src/App.js
+++ src/App.js
@@ -6,19 +6,16 @@ import ProtectedRoute from './components/ProtectedRoute'
 import MyProfile from './components/MyProfile'
 import UserProfile from './components/UserProfile'
 import NotFound from './components/NotFound'
-import {SearchProvider} from './context/SearchContext'
 
 const App = () => (
   <>
-    <SearchProvider>
-      <Switch>
-        <Route exact path="/login" component={Login} />
-        <ProtectedRoute exact path="/" component={Home} />
-        <ProtectedRoute exact path="/my-profile" component={MyProfile} />
-        <ProtectedRoute exact path="/users/:id" component={UserProfile} />
-        <Route component={NotFound} />
-      </Switch>
-    </SearchProvider>
+    <Switch>
+      <Route exact path="/login" component={Login} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute exact path="/my-profile" component={MyProfile} />
+      <ProtectedRoute exact path="/users/:id" component={UserProfile} />
+      <Route component={NotFound} />
+    </Switch>
   </>
 )
 
